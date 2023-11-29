c15dd6eba643cab1097cce4cea48afa034cf78cd
earocorn <amavb789@gmail.com>
2023-11-29 11:52:05 -0600

    correct

--- src/wordjourney/view/components/LeaderBoardView.java
+++ src/wordjourney/view/components/LeaderBoardView.java
@@ -45,13 +45,15 @@ public class LeaderBoardView extends JPanel {
     public void updateLeaderboardText() {
         if(lbColumns != null && lbColumns.length >= 5) {
 
-            // 2D array to store leaderboard entries (player name and score)
-            String[][] leaderboardArray = new String[5][2];
+            // 2D array to store leaderboard entries (player name and score
 
             // Populate the array with data from the game's leaderboard
             int i = 0;
+            int setSize = GameUtility.getInstance().getLeaderboardData().getLeaderboard().entrySet().size();
+            String[][] leaderboardArray = new String[setSize][2];
+            System.out.println("SET SIZE = " + setSize);
             for (Map.Entry<String, Long> entry : GameUtility.getInstance().getLeaderboardData().getLeaderboard().entrySet()) {
-                if (i < 5) {
+                if (i < setSize) {
                     leaderboardArray[i][0] = entry.getKey();
                     leaderboardArray[i][1] = String.valueOf(entry.getValue());
                     System.out.println(leaderboardArray[i][0] + " : " + leaderboardArray[i][1]);
@@ -61,7 +63,7 @@ public class LeaderBoardView extends JPanel {
                 }
 
             }
-            while (i < 5) {
+            while (i < setSize) {
                 leaderboardArray[i][0] = "";
                 leaderboardArray[i][1] = "";
                 System.out.println(leaderboardArray[i][0] + " : " + leaderboardArray[i][1]);
@@ -69,7 +71,7 @@ public class LeaderBoardView extends JPanel {
             }
             Arrays.sort(leaderboardArray, Comparator.comparing(row -> !row[1].isEmpty() ? Long.parseLong((String) row[1]) : 0, Comparator.reverseOrder()));
             // populates the leaderboard and sets the data with fonts, colors, etc
-            for (int j = 0; j < 5; j++) {
+            for (int j = 0; j < setSize; j++) {
                 lbColumns[j].setFont(GameUtility.getFont());
                 String text = leaderboardArray[j][0] + "-" + leaderboardArray[j][1];
                 lbColumns[j].setText(text);
